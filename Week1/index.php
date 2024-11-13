7d18155cece4f2627d417cf82826430f5bfe86c8
Junwei <laujunwei1220@e.neivce.edu.my>
2024-11-13 13:05:33 +0800

    try

---
+++ Week1/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Frist PHP</title>
+    </head>
+    <body bgcolor="pink">
+        <?php 
+            echo "Junwei";
+        ?>
+    </body>
+</html>
